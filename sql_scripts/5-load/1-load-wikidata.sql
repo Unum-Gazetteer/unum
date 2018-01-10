f4960d83547cd5d8165094a680fca6dcef86acea
DanielJDufour <daniel.j.dufour@gmail.com>
2018-01-10 06:30:40 +0000

    updated load wikidata to work

--- sql_scripts/5-load/1-load-wikidata.sql
+++ sql_scripts/5-load/1-load-wikidata.sql
@@ -1,14 +1,14 @@
 CREATE TABLE wikidata (
     primary_name varchar(1000),
     enwiki_title varchar(1001),
-    other_names varchar(10000),
+    other_names varchar(20000),
     country varchar(500),
     classes varchar(1002),
     elevation varchar(100),
     geonames_id varchar(100),
     latitude float,
     longitude float,
-    population integer
+    population varchar(100)
 );
 
-COPY wikidata FROM '/tmp/gazetteers/wikidata-gazetteer.tsv' WITH (FORMAT 'csv', DELIMITER E'\t');
+COPY wikidata FROM '/tmp/gazetteers/wikidata-gazetteer.csv' WITH (FORMAT 'csv', DELIMITER E'\t');
