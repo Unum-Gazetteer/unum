3f5cb677865f72e946f68df648daeb2f0de140ab
DanielJDufour <daniel.j.dufour@gmail.com>
2018-01-13 04:55:05 +0000

    switched drop table to drop table exists

--- sql_scripts/5-load/5-create_place_type_mappings.sql
+++ sql_scripts/5-load/5-create_place_type_mappings.sql
@@ -1,4 +1,4 @@
-DROP TABLE geoname_feature_class_to_place_type;
+DROP TABLE IF EXISTS geoname_feature_class_to_place_type;
 
 CREATE TABLE geoname_feature_class_to_place_type (
     geoname_feature_class varchar(1),
@@ -9,7 +9,7 @@ COPY geoname_feature_class_to_place_type FROM '/tmp/geoname_feature_class_to_pla
 
 
 
-DROP TABLE osm_class_to_place_type;
+DROP TABLE IF EXISTS osm_class_to_place_type;
 
 CREATE TABLE osm_class_to_place_type (
     osm_class varchar(10),
@@ -21,10 +21,10 @@ COPY osm_class_to_place_type FROM '/tmp/osm_class_to_place_type.csv' WITH (DELIM
 
 
 
-DROP TABLE osm_type_to_place_type;
+DROP TABLE IF EXISTS osm_type_to_place_type;
 
 CREATE TABLE osm_type_to_place_type (
-    osm_type varchar(15),
+    osm_type varchar(25),
     place_type varchar(1)
 );
 
