f70955cdd9980f6524f35e6aa7552cbb6058a266
DanielJDufour <daniel.j.dufour@gmail.com>
2018-02-11 07:26:53 +0000

    added conflated by geonames id

---
+++ sql_scripts/20-conflate/3-conflate-by-geonames-id.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS same_geonames_id;
+
+
+CREATE TABLE same_geonames_id (
+    id serial primary key,
+    place_ids varchar(10000),
+    geonameid varchar(107),
+    instance_count int
+);
+
+
+INSERT INTO same_geonames_id (place_ids, geonameid, instance_count)
+SELECT array_to_string(array_agg(id), ','), geonameid, count(*)
+FROM places
+WHERE geonameid IS NOT NULL AND geonameid != ''
+GROUP BY geonameid, admin_level;
+
+
+DELETE FROM same_geonames_id WHERE instance_count = 1;
+
+
+SELECT count(*) from same_geonames_id; 
+
+
+SELECT count(*) AS num_places from places;
+
+
+SELECT count(conflate_places_by_ids(place_ids)) FROM same_geonames_id;
+
+
+SELECT count(*) AS num_places from places;
+
+
+DELETE
+FROM places
+WHERE id IN (SELECT unnest(string_to_array(string_agg(place_ids, ','), ',')::bigint[]) FROM same_geonames_id);
+
+
+SELECT count(*) AS num_places from places;
