b62e8aebd99a4048008b461777f867448b1f73c5
DanielJDufour <daniel.j.dufour@gmail.com>
2018-02-13 06:16:59 +0000

    updated

--- sql_scripts/create_extensions.sql
+++ sql_scripts/create_extensions.sql
@@ -2,3 +2,6 @@ CREATE EXTENSION unaccent;
 CREATE EXTENSION file_fdw;
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
+
+/* manually downloaded and made */
+CREATE EXTENSION safecast;
