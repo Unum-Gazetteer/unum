219c3e9167cf632ed260369affa50d981aeb24c9
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2019-09-15 19:38:42 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,95 +1,95 @@
-# Update Package Lists
+echo "Update Package Lists"
 sudo apt-get update -qq
 
-# Install Database Dependencies
+echo "Install Database Dependencies"
 sudo apt-get install -y postgresql postgresql-contrib postgresql-server-dev-all '^postgresql-[0-9].[0-9]-postgis-[0-9].[0-9]$'
 
-# Install Other System Dependencies
+echo "Install Other System Dependencies"
 sudo apt-get install -y build-essential git sudo unzip vim zip
 
-# Restart PostgreSQL
+echo "Restart PostgreSQL"
 sudo service postgresql restart
 
-# Create Postgresql USER
+echo "Create Postgresql USER"
 sudo -u postgres psql -c "CREATE ROLE $(whoami) CREATEDB LOGIN SUPERUSER"
 sudo -u postgres psql -c "CREATE DATABASE unum;"
 
-# Download Safecast
+echo "Download Safecast"
 cd /tmp && git clone https://github.com/DanielJDufour/safecast
 
-# Install Safecast
+echo "Install Safecast"
 cd /tmp/safecast && make install
 
-# Initialize DB Extensions
+echo "Initialize DB Extensions"
 psql -f sql_scripts/create_extensions.sql unum;
 
-# Create PSQL Utils
+echo "Create PSQL Utils"
 for file in sql_scripts/1-utils/*; do
   psql -f $file unum;
 done
 
-# Create Custom Number Conversion Functions
+echo "Create Custom Number Conversion Functions"
 for file in sql_scripts/2-convert/*; do
   psql -f $file unum;
 done
 
-# Create gazetteers Directory
+echo "Create gazetteers Directory"
 if [ ! -d "/tmp/gazetteers" ] ; then
   mkdir /tmp/gazetteers;
 fi;
 
-# Download Gazetteer from Wikidata
+echo "Download Gazetteer from Wikidata"
 if [ ! -f "/tmp/gazetteers/wikidata-gazetteer.tsv" ] ; then
   cd /tmp/gazetteers;
   wget https://s3.amazonaws.com/firstdraftgis/wikidata-gazetteer.tsv.zip;
   unzip wikidata-gazetteer.tsv.zip;
 fi;
 
-# Download OSMNames
+echo "Download OSMNames"
 if [ ! -f "/tmp/gazetteers/planet-latest_geonames.tsv" ] ; then
   cd /tmp/gazetteers;
   wget https://github.com/OSMNames/OSMNames/releases/download/v2.0.3/planet-latest_geonames.tsv.gz;
   gunzip planet-latest_geonames.tsv.gz;
 fi;
 
-# Download GeoNames
+echo "Download GeoNames"
 if [ ! -f "/tmp/gazetteers/allCountries.txt" ] ; then
   cd /tmp/gazetteers;
   wget http://download.geonames.org/export/dump/allCountries.zip;
   unzip allCountries.zip;
-fi;  
+fi;
 
-# Copy Over Temp Files
+echo "Copy Over Temp Files"
 cp data/* /tmp/.
 
-# Load Gazetteers
+echo "Load Gazetteers"
 for file in sql_scripts/5-load/*; do
   psql -f $file unum;
 done
 
-# Re-format Data from Gazetteers into Standard Format
+echo "Re-format Data from Gazetteers into Standard Format"
 for file in sql_scripts/10-conform/*; do
   psql -f $file unum;
 done
 
-# Conflate
+echo "Conflate"
 for file in sql_scripts/20-conflate/*; do
   psql -f $file unum;
 done
 
-# Export
+echo "Export"
 for file in sql_scripts/30-export/*; do
   psql -f $file unum;
 done
 
-# Sample
+echo "Sample"
 head -1 /tmp/unum.tsv > /tmp/unum_sample.tsv && time shuf -n 10000 /tmp/unum.tsv >> /tmp/unum_sample.tsv
 
-# Install Pandas
+echo "Install Pandas"
 pip install pandas --upgrade
 
-# Test
+echo "Test"
 python3 test.py
 
-# Zip
+echo "Zip"
 cd /tmp && zip -r unum.tsv.zip unum.tsv
