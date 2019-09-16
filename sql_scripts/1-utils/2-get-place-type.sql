aa430abdc40d4401c976ab22d69fe111acfd3d49
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2019-09-15 21:06:54 -0400

    Update 2-get-place-type.sql

--- sql_scripts/1-utils/2-get-place-type.sql
+++ sql_scripts/1-utils/2-get-place-type.sql
@@ -4,7 +4,7 @@ AS $$
 BEGIN
     IF osmname_type = 'administrative' OR geonames_feature_class = 'A' THEN
         RETURN 'admin';
-    ELSIF geonames_feature_class = osmname_type
+    ELSIF geonames_feature_class = osmname_type THEN
         RETURN NULL;
     END IF;
 EXCEPTION when others THEN
